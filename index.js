a66f91b7c856673dc4ac347eed8b87d28e8e7483
myFirstgitrepo <tanvirmahin607@gmail.com>
2024-12-05 20:21:47 +0600

    conected with mongodb with client side and added a post and get method

--- index.js
+++ index.js
@@ -1,11 +1,56 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port =  process.env.PORT || 5000;
 const app = express();
 
 // middle ware 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kriop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const reviewsCollection = client.db("GameHeavenDB").collection("reviews");
+
+    //get all data from mongodb
+    app.get('/reviews', async(req, res)=>{
+        const query = reviewsCollection.find();
+        const result = await query.toArray();
+        res.send(result);
+    })
+
+    //  get data from client site and send data to mongodb
+      app.post('/reviews', async (req, res) => {
+        const newReview = req.body;
+        const result = await reviewsCollection.insertOne(newReview);
+        res.send(result);
+      })
+
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 
